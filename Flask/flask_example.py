af67f80179f439c4823426ef31ef1ac7ccb5f332
RodrighoNS <rnavarro@lemontech.com>
2021-06-22 22:30:36 -0400

    Add SQLAlchemy object to connect to Postgres

--- Flask/flask_example.py
+++ Flask/flask_example.py
@@ -1,7 +1,14 @@
 from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# Postgres config variables              dialect     username pass host      port database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost:5432/example'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # avoid warning
+
+# Open a connection
+db = SQLAlchemy(app)
 
 @app.route('/') #Python decorator
 def index():
